1d81c3b90b2d7b91c9c615d70ad49e3131a46a86
bogeholm <troelsmikkelsen@gmail.com>
2018-12-27 23:19:28 +0100

    added Dockerfile for building for ARM

---
+++ Docker/build-for-arm.sh
@@ -0,0 +1,13 @@
+# https://docs.docker.com/engine/reference/commandline/build/#parent-command
+docker build -t raspberry-build .
+
+# https://hackernoon.com/seamlessly-cross-compiling-rust-for-raspberry-pis-ede5e2bd3fe2
+# https://matthiasnoback.nl/2017/04/docker-build-patterns/
+# https://stackoverflow.com/questions/8426058/getting-the-parent-of-a-directory-in-bash
+docker run \
+	--name docker-tutorial \
+	--rm \
+	--mount type=bind,source="$(dirname "$(pwd)")",target=/app \
+	--mount type=bind,source="$(echo $HOME)"/raspberry-build/cargo/registry,target=/usr/local/cargo/registry \
+	--shm-size=1g \
+	raspberry-build
