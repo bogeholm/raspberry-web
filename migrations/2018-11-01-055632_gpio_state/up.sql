4fe3495f779b8cce890367fb8dbe51471567da13
bogeholm <troelsmikkelsen@gmail.com>
2019-04-06 00:06:05 +0200

    Update GPIOs that can be used

--- migrations/2018-11-01-055632_gpio_state/up.sql
+++ migrations/2018-11-01-055632_gpio_state/up.sql
@@ -6,7 +6,7 @@ CREATE TABLE gpio_state (
 	gpio_level	TEXT,
 	last_change	TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+-- Based on https://www.raspberrypi.org/documentation/usage/gpio/ & https://pinout.xyz/#
 INSERT INTO gpio_state (
     gpio_id,
     in_use
@@ -29,15 +29,14 @@ VALUES
     (14, 0),
     (15, 0),
     (16, 0),
-    -- GPIO 17 - 20 do not exist
+    (17, 0),
+    (18, 0),
+    (19, 0),
+    (20, 0),
     (21, 0),
     (22, 0),
     (23, 0),
     (24, 0),
     (25, 0),
     (26, 0),
-    (27, 0),
-    (28, 0),
-    (29, 0),
-    (30, 0),
-    (31, 0);
+    (27, 0);
